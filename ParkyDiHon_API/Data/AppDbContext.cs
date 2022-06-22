c0c38b79938b81954e574626bb113ff959da20c6
908119 <908119@LTIN171760>
2022-06-22 09:08:50 +0530

    Add project files.

---
+++ ParkyDiHon_API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyDiHon_API.Models;
+
+namespace ParkyDiHon_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+                
+        }
+        public DbSet<NationalPark> nationalParks { get; set; }
+    }
+}
---
+++ ParkyDiHon_API/Models/NationalPark.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkyDiHon_API.Models
+{
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Establised { get; set; }
+    }
+}
